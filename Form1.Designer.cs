06a8acdf754769332252f60409c1d5c4a454c233
ASUS <ASUS@TANHUG>
2024-03-12 14:34:54 +0700

    Revert to commit 8372253

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,26 +31,25 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtpassword = new System.Windows.Forms.TextBox();
-            this.btnlogin = new System.Windows.Forms.Button();
+            this.txtMauKhau = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 105);
+            this.label1.Location = new System.Drawing.Point(44, 107);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Tên Đăng Nhập";
+            this.label1.Text = "UserName";
             // 
             // txtUserName
             // 
             this.txtUserName.Location = new System.Drawing.Point(125, 102);
             this.txtUserName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.Size = new System.Drawing.Size(83, 20);
             this.txtUserName.TabIndex = 1;
             // 
             // label2
@@ -61,33 +60,20 @@
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Mật Khẩu";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // txtpassword
+            // txtMauKhau
             // 
-            this.txtpassword.Location = new System.Drawing.Point(125, 166);
-            this.txtpassword.Name = "txtpassword";
-            this.txtpassword.Size = new System.Drawing.Size(100, 20);
-            this.txtpassword.TabIndex = 3;
-            // 
-            // btnlogin
-            // 
-            this.btnlogin.Location = new System.Drawing.Point(125, 216);
-            this.btnlogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnlogin.Name = "btnlogin";
-            this.btnlogin.Size = new System.Drawing.Size(83, 25);
-            this.btnlogin.TabIndex = 4;
-            this.btnlogin.Text = "Đăng nhập";
-            this.btnlogin.UseVisualStyleBackColor = true;
-            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            this.txtMauKhau.Location = new System.Drawing.Point(125, 166);
+            this.txtMauKhau.Name = "txtMauKhau";
+            this.txtMauKhau.Size = new System.Drawing.Size(100, 20);
+            this.txtMauKhau.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Controls.Add(this.btnlogin);
-            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtMauKhau);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.label1);
@@ -104,8 +90,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtUserName;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtpassword;
-        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.TextBox txtMauKhau;
     }
 }
 
